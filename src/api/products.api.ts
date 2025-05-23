2839178b3523e874f143283e7d8d965b1323e9cd
ashutosh229 <akumarjha875@gmail.com>
2025-05-23 20:02:12 +0530

    initial push for the APIs building process

--- src/api/products.api.ts
+++ src/api/products.api.ts
@@ -1,1 +1,10 @@
-export const fetchProducts = async (): Promise<>
+import type { Product } from "../types/products";
+
+export const fetchProducts = async (): Promise<Product[]> => {};
+
+export const updateProductTitle = async (
+  id: number,
+  title: string
+): Promise<Product> => {};
+
+export const deleteProduct = async (id: number): Promise<void> => {};
