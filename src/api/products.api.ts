42239ce6d1f6bcf525a432d614fa4f99eb71e4ce
ashutosh229 <akumarjha875@gmail.com>
2025-05-23 20:10:11 +0530

    build the API for fetching the products

--- src/api/products.api.ts
+++ src/api/products.api.ts
@@ -1,6 +1,31 @@
 import type { Product } from "../types/products";
 
-export const fetchProducts = async (): Promise<Product[]> => {};
+const DELAY = 1000;
+let products: Product[] = [];
+
+export const fetchProducts = async (): Promise<Product[]> => {
+  try {
+    if (products.length > 0) {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve([...products]), DELAY);
+      });
+    }
+    const response = await fetch("https://dummyjson.com/products");
+    if (!response.ok) {
+      //error handling to be done
+    }
+    const data = await response.json();
+    if (!data || !data.products) {
+      //error handling to be done
+    }
+    products = data.products;
+    return new Promise((resolve) => {
+      setTimeout(() => resolve([...products]), DELAY);
+    });
+  } catch (error) {
+    //error handling to be done
+  }
+};
 
 export const updateProductTitle = async (
   id: number,
